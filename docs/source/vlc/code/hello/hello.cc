07e6bc7fb8b23845b1dc8eda3ca464dd0ede8fc8
Fangjun Kuang <fangjun.kuang@gmail.com>
2025-06-08 16:17:29 +0800

    small fixes

--- docs/source/vlc/code/hello/hello.cc
+++ docs/source/vlc/code/hello/hello.cc
@@ -1,9 +1,12 @@
 #include "vlc/libvlc.h"
-#include <iostream>
 #include <stdio.h>
 
 int main() {
   const char *version = libvlc_get_version();
+  const char *compiler_version = libvlc_get_compiler();
+  const char *change_set = libvlc_get_changeset();
   printf("libvlc version: %s\n", version);
+  printf("compiler version: %s\n", compiler_version);
+  printf("changeset: %s\n", change_set);
   return 0;
 }
